312fb79a896b379a4aa6e640648c9db945d6148c
chenxiccc <chenxiccc@gmail.com>
2020-02-16 12:48:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,10 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.31.200/g' package/base-files/files/bin/config_generate
-wget -O package/network/services/dnsmasq/files/dhcp.conf https://raw.githubusercontent.com/chenxiccc/Customfiles/master/x86/dhcp
-wget -O package/base-files/files/etc/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
-wget -O package/network/config/firewall/files/firewall.config https://raw.githubusercontent.com/chenxiccc/Customfiles/master/x86/firewall
+wget -O tmp/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
+wget -O tmp/dhcp.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/x86/dhcp
+cat tmp/dhcp.tmp >> package/network/services/dnsmasq/files/dhcp.conf
+rm tmp/dhcp.tmp
+wget -O tmp/firewall.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/x86/firewall
+cat tmp/firewall.tmp >> package/network/config/firewall/files/firewall.config
+rm tmp/firewall.tmp
