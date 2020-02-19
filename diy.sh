5490e73cd4ca2774e4525022b888006a56797739
chenxiccc <chenxiccc@gmail.com>
2020-02-20 02:21:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 wget https://raw.githubusercontent.com/chenxiccc/Customfiles/master/dlpackage.sh
 chmod +x dlpackage.sh
-../dlpackage.sh
+sh dlpackage.sh
 sed -i 's/192.168.1.1/192.168.31.200/g' package/base-files/files/bin/config_generate
 wget -O package/base-files/files/etc/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
 wget -O tmp/dhcp.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/x86/dhcp
