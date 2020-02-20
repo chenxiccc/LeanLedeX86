484e1d2dbc019f5f3f82a2c98eb5fd037665ff7b
chenxiccc <chenxiccc@gmail.com>
2020-02-20 14:42:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,3 +18,7 @@ rm tmp/dhcp.tmp
 wget -O tmp/firewall.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/x86/firewall
 cat tmp/firewall.tmp >> package/network/config/firewall/files/firewall.config
 rm tmp/firewall.tmp
+wget -O package/myapp/passwall/luci-app-passwall/root/etc/config/passwall_rule/blacklist_host https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/blacklist_host
+wget -O package/myapp/passwall/luci-app-passwall/root/etc/config/passwall_rule/whitelist_host https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/whitelist_host
+wget -O package/myapp/passwall/luci-app-passwall/root/etc/config/passwall_rule/blacklist_ip https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/blacklist_ip
+wget -O package/myapp/passwall/luci-app-passwall/root/etc/config/passwall_rule/whitelist_ip https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/whitelist_ip
